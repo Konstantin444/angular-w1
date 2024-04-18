ef51af69dc6e80140705d339d68254bba4a5e677
Konstantin444 <kostadinov.main@gmail.com>
2024-04-18 20:14:33 +0300

    First commit

--- hello-world-2201681068/src/app/app.component.ts
+++ hello-world-2201681068/src/app/app.component.ts
@@ -1,13 +1,38 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, MatExpansionModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'hello-world-2201681068';
+
+  //Инфо за Борко
+  personalInfo = {
+    name: "Борко Борев",
+    age: 21,
+    profession: "Software Developer"
+  };
+
+  //Хобита
+  hobbies = ["Дискотеки", "Фитнес", "Наргиле Барове"];
+
+  //Линк
+  favoriteCreatorLink = 'https://example.com';
+
+  //Инфо за канала на Борко
+  channelInfo = {
+    name: "Borko Mafia",
+    mission: "Споделяне на съвременни житейски мъдрости",
+    expectation: "Очаквайте курсове, туториали и дискусии за достигането на статус TOP G."
+  };
+
+  //Остойностяване
+  servicePricing = 'Цените на Discord курсовете ми започват от 150 лв. на месец, а може също да си закупите книгата ми за 29.99 лв.';
 }
